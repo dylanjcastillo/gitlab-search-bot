1d52b13f215c1968be06dcf975ae10436baa307c
Dylan Castillo <dylanjcastillo@gmail.com>
2023-03-13 12:48:19 +0100

    Add parser, processor, and vectorizer

--- gitlab_search_bot/config.py
+++ gitlab_search_bot/config.py
@@ -1,6 +1,11 @@
+import os
 from pathlib import Path
 
 from dataclasses import dataclass
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 @dataclass
@@ -8,3 +13,8 @@ class Paths:
     root: Path = Path(__file__).parent.parent.parent
     data: Path = root / "data"
     handbook: Path = data / "handbook"
+
+
+@dataclass
+class APIKeys:
+    cohere: str = os.getenv("COHERE_API_KEY", "")
