07cde731c3cc0466e94ca7e4bbf360927a218044
Dylan Castillo <dylanjcastillo@gmail.com>
2023-03-14 01:52:27 +0100

    Full working version

--- gitlab_search_bot/config.py
+++ gitlab_search_bot/config.py
@@ -10,11 +10,24 @@ load_dotenv()
 
 @dataclass
 class Paths:
-    root: Path = Path(__file__).parent.parent.parent
+    root: Path = Path(__file__).parent.parent
     data: Path = root / "data"
-    handbook: Path = data / "handbook"
 
 
 @dataclass
-class APIKeys:
-    cohere: str = os.getenv("COHERE_API_KEY", "")
+class ModelsConfig:
+    reranking_model_name: str = "cross-encoder/ms-marco-MiniLM-L-6-v2"
+
+
+@dataclass
+class CohereConfig:
+    api_key: str = os.getenv("COHERE_API_KEY", "")
+    vector_size: int = 4096
+
+
+@dataclass
+class QdrantConfig:
+    api_key: str = os.getenv("QDRANT_API_KEY", "")
+    host: str = os.getenv("QDRANT_HOST", "localhost")
+    port: int = int(os.getenv("QDRANT_PORT", 6333))
+    collection_name: str = "gitlab-search-bot"
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./gitlab_search_bot/app/templates/*.html", "./gitlab_search_bot/app/*.py"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
